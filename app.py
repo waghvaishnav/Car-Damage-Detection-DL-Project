139bf046c131465a03bca56b2cf7d9260f1f23d6
waghvaishnav <waghvaishnav29@gmail.com>
2026-04-18 20:16:45 +0530

    code push

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from model_helper import predict
+
+st.title("Vehicle Damage Detection")
+
+uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
+
+if uploaded_file:
+    image_path = "temp_file.jpg"
+    with open(image_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
+        prediction = predict(image_path)
+        st.info(f"Predicted Class: {prediction}")
